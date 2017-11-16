ad1ae29b57a10c725cb23a028bf2e246549830a9
Dennis Hodges <fermentationist@dhcp-10-102-226-169.wireless.northwestern.private>
2017-11-15 19:49:13 -0600

    replaced individual html routes with 'express.static'

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
-=======
 //=========dependencies==========//
->>>>>>> caa311f59b182e782f9f149a1903e7db75fb1d22
 const express = require ("express");
 const bodyParser = require ("body-parser");
 const path = require ("path");
 
-<<<<<<< HEAD
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({extended : false}));
-app.use(bodyParser.json());
-
-=======
 //=========initialize express server=========//
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,33 +10,24 @@ const PORT = process.env.PORT || 3000;
 //==========configure middleware============//
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
+app.use(express.static("./"));
 
-//======html endpoints========//
->>>>>>> caa311f59b182e782f9f149a1903e7db75fb1d22
-app.get("/", function(req, res){
-	res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/reservations", function(req, res){
-	res.sendFile(path.join(__dirname, "reservations.html"));
-});
-
-app.get("/tables", function(req, res){
-	res.sendFile(path.join(__dirname, "tables.html"));
-});
-
-<<<<<<< HEAD
-
-
-
-
-
-=======
 //======json endpoints========//
-app.get("/api/all", function(req, res){
-	res.json(tables, waitlist);
+app.get("/api/tables", function(req, res){
+	res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res){
 	res.json(waitlist);
-})
+});
+
+
+
+
+
+
+
+
 
 //========data structures=======//
 let tables = [
@@ -71,7 +51,6 @@ let waitlist = [
 
 
 //=========start server=========//
->>>>>>> caa311f59b182e782f9f149a1903e7db75fb1d22
 app.listen(PORT, function(){
 	console.log("Listening on port", PORT);
 })
