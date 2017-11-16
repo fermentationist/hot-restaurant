fa45ad78df926817470b4d2ff824022b4e2a4d6c
mkdoh3 <mkdoherty3@GMAIL.COM>
2017-11-15 21:29:45 -0600

    stuff

--- reservations.js
+++ reservations.js
@@ -1,1 +1,22 @@
 console.log("reservations.js linked!");
+
+
+
+$("#resoSubmit").on("click", function (event) {
+    event.preventDefault();
+    let formData = {
+        name: $('#nameInput').val().trim(),
+        phone: $('#phoneInput').val().trim(),
+        email: $('#emailInput').val().trim(),
+        customerID: $("idInput").val().trim()
+    };
+    let location = window.location.origin;
+    $.post(location + '/api/add', formData, function (data) {
+        console.log(data)
+        if (data) {
+            alert(data)
+        }
+        $(".card-body").append(JSON.stringify(formData))
+
+    })
+})
